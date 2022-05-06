d3ffa84b471bfccd65d9a0e478934ba389e325ae
hossein <hosseinbidaki79@gmail.com>
2022-05-06 14:54:32 +0430

    14:54

---
+++ A.cpp
@@ -0,0 +1,16 @@
+//
+// Created by hossein on 5/6/2022.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n, 0);
+    for (auto it : a) {
+        cout << it << " \n";
+    }
+}
